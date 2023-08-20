e053b32fd633ca4f578bf10a456d79c8a56eb885
mai-cs50 <maimohamed.1992@yahoo.com>
2023-08-20 08:18:16 -0700

    d

--- main.h
+++ main.h
@@ -13,7 +13,26 @@
 #define CONVERT_LOWERCSE
 #define CONVERT_UNSIGNED
 
+/**
+ *
+ *
+ *
+ *
+*/
+typedef struct parameters
+{
+	unsigned int unsign	: 1;
+	unsigned int plus_flag	: 1;
+	unsigned int space_flag	: 1;
+	unsigned int hashtag_flag	: 1;
+	unsigned int zero_flag	: 1;
+	unsigned int minus_flag	: 1;
 
+	unsigned int width;
+	unsigned int precision;
+	unsigned int h_modifier;
+	unsigned int l_modifier;
+} params_t;
 /**
  * struct format - match the conversion specifiers for printf
  * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
@@ -63,32 +82,8 @@ int print_number_left_shift(char *str, params_t *params);
 
 void init_params(va_list ap, params_t *params);
 
-char *get_precision(char *p, va_list ap, params_t *params);
+char *get_precision(char *p, params_t *params, va_list ap)
 
 int _printf(const char *format, ...)
-
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int rev_string(char *s);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _putchar(char c);
-int _printf(const char *format, ...);
 
 #endif
--- specifier.c
+++ specifier.c
@@ -8,7 +8,7 @@
  *
  *
 */
-int(*get_specifier(char *s))(va_list ap, params_t *params)
+int (*get_specifier(char *s))(va_list ap, params_t *params)
 {
 	specifier_t specifiers[] = {
 		{"c", print_char},
@@ -37,4 +37,70 @@ int(*get_specifier(char *s))(va_list ap, params_t *params)
 		i++;
 	}
 	return (NULL);
+}
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+*/
+int get_print_func(char *s, va_list ap, params_t *params)
+{
+	int (*f) (va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
+}
+int get_flag(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case '+':
+			i = params->plus_flag = 1;
+			break;
+		case ' ':
+			i = params->space_flag = 1;
+			break;
+		case '-':
+			i = params->minus_flag = 1;
+			break;
+		case '#':
+			i = params->hashtag_flag = 1;
+			break;
+		case '0':
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (i);
+}
+/**
+ *
+ *
+ *
+ *
+ *
+*/
+char *get_width(char *s, params_t *params, va_list ap)
+{
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->width = d;
+	return (s);
 }
