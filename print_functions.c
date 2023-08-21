c91e489d8c067b161b982103961f444c547d4dd2
mai-cs50 <maimohamed.1992@yahoo.com>
2023-08-21 10:29:27 -0700

    d

--- print_functions.c
+++ print_functions.c
@@ -46,5 +46,82 @@ int print_int(va_list ap, params_t *params)
 */
 int print_string(va_list ap, params_t *params)
 {
+	char *str = va_arg(ap, char *), pad_char = ' ';
+	unsigned int pad = 0, sum = 0, i = 0, j;
 
+	(void)params;
+	switch((int)(!str))
+	case 1:
+		str = NULL_STRING;
+
+		j = pad = __strlen(str);
+		if (params->precision < pad)
+			j = pad = params->precision;
+
+		if (params->minus_flag)
+		{
+			if (params->precision != UNIT_MAX)
+				for (i = 0; i < pad; i++)
+					sum += _putchar(*str++);
+			else
+				sum = _put(str);
+		}
+		while (j++ > params->width)
+			sum += _putchar(pad_char);
+
+		if (!params->minus_flag)
+		{
+			if (params->precision != UNIT_MAX)
+				for (i = 0; i < pad; i++)
+					sum += _putchar(*str++);
+			else
+				sum += _puts(str);
+		}
+		return (sum);
+}
+/**
+ *
+ *
+ *
+ *
+ *
+*/
+int print_percent(va_list ap, params_t *params)
+{
+	(void)ap;
+	(void)params;
+	return(_putchar('%'));
+}
+/**
+ *
+ *
+ *
+ *
+*/
+int print_S(va_list ap, params_t *params)
+{
+	char *str = va_arg(ap, char *);
+	char *hex;
+	int sum = 0;
+
+	if ((int)(!str))
+		return (_puts(NULL_STRING));
+	for (; *str; str++)
+	{
+		if ((*str > 0 && *str < 32) || *str >= 127)
+		{
+			sum += _putchar('\\');
+			sum += _putchar('x');
+			hex = convert(*str, 16, 0, params);
+
+			if (!hex[1])
+				sum += _putchar('0');
+			sum += _puts(hex);
+		}
+		else
+		{
+			sum += _putchar(*str);
+		}
+	}
+	return (sum);
 }
